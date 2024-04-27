62006aabeb42c15c710dcb8fb7ebbe64ebe252f2
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-04-27 13:10:50 +0700

    Design Website

--- src/app/components/fragments/Homepage/atas.tsx
+++ src/app/components/fragments/Homepage/atas.tsx
@@ -30,7 +30,7 @@ export default function Atas() {
               height={500}
             ></Image>
           </main>
-          <main className="flex flex-col border-[4px] px-4 border-gray-100 shadow-xl  w-full min-h-screen pt-4">
+          <main className="flex flex-col w-full min-h-screen pt-4 mb-5">
             <h1 className="text-left text-2xl font-semibold w-full mb-10">
               Kategori Pilihan
             </h1>
@@ -92,28 +92,100 @@ export default function Atas() {
             </div>
           </main>
           <main className="w-full">
-            <div className="flex ">
-              <h1 className="text-left text-2xl font-semibold mb-5">
+            <div className="flex items-end gap-3 mb-3">
+              <h1 className="text-left text-2xl font-semibold">
                 Kejar Diskon Spesial
               </h1>
-              <p>Berakhir dalam</p>
-              <span>04 : 00 : 00</span>
+              <p className="ml-4 text-gray-700">Berakhir dalam </p>
+              <span className="flex gap-2 text-#A0153E font-semibold items-center">
+                <p className="p-2 rounded-md w-10 text-center bg-[#A0153E] text-white">
+                  04
+                </p>
+                :
+                <p className="p-2 rounded-md w-10 text-center bg-[#A0153E] text-white">
+                  53
+                </p>
+                :
+                <p className="p-2 rounded-md w-10 text-center bg-[#A0153E] text-white">
+                  10
+                </p>
+              </span>
             </div>
             <div>
-              <div className="flex gap-5">
-                <span className="w-[17rem] h-[20rem] bg-slate-500">
+              <div className="flex gap-10">
+                <div className="w-[17rem] h-[20rem] shadow-xl flex flex-col">
                   <Image
-                    className="h-64"
+                    className="h-48 w-full object-cover mx-auto"
                     src="/images/diskon_sepatu.png"
                     alt="Image Discount"
                     width={500}
                     height={500}
                   ></Image>
-                  <p>Rp. 1.000.000</p>
-                  <p>Rp. 1.000.000</p>
-                </span>
-                <div className="w-[17rem] h-[20rem] bg-slate-500"></div>
-                <div className="w-[17rem] h-[20rem] bg-slate-500"></div>
+                  <div className="pl-3 flex flex-col gap-7">
+                    <span className="flex flex-col">
+                      <p className="font-bold text-xl">Rp. 1.000.000</p>
+                      <p className="text-gray-700">
+                        Rp. 1.000.000{" "}
+                        <span className="text-[#A0153E] font-bold ml-1">
+                          40%
+                        </span>
+                      </p>
+                    </span>
+                    <span className="h-2 bg-gray-300 rounded-lg w-[95%] block">
+                      <span className="block w-[63%] h-2 justify-center bg-[#A0153E] rounded-lg"></span>
+                    <p className="text-gray-500 text-sm">Tersedia</p>
+                    </span>
+                  </div>
+                </div>
+                <div className="w-[17rem] h-[20rem] shadow-xl flex flex-col">
+                  <Image
+                    className="h-48 w-full object-cover mx-auto"
+                    src="/images/diskon_sepatu.png"
+                    alt="Image Discount"
+                    width={500}
+                    height={500}
+                  ></Image>
+                  <div className="pl-3 flex flex-col gap-7">
+                    <span className="flex flex-col">
+                      <p className="font-bold text-xl">Rp. 1.000.000</p>
+                      <p className="text-gray-700">
+                        Rp. 1.000.000{" "}
+                        <span className="text-[#A0153E] font-bold ml-1">
+                          40%
+                        </span>
+                      </p>
+                    </span>
+                    <span className="h-2 bg-gray-300 rounded-lg w-[95%] block">
+                      <span className="block w-[63%] h-2 justify-center bg-[#A0153E] rounded-lg"></span>
+                    <p className="text-gray-500 text-sm">Tersedia</p>
+                    </span>
+                  </div>
+                </div>
+                <div className="w-[17rem] h-[20rem] shadow-xl flex flex-col">
+                  <Image
+                    className="h-48 w-full object-cover mx-auto"
+                    src="/images/diskon_sepatu.png"
+                    alt="Image Discount"
+                    width={500}
+                    height={500}
+                  ></Image>
+                  <div className="pl-3 flex flex-col gap-7">
+                    <span className="flex flex-col">
+                      <p className="font-bold text-xl">Rp. 1.000.000</p>
+                      <p className="text-gray-700">
+                        Rp. 1.000.000{" "}
+                        <span className="text-[#A0153E] font-bold ml-1">
+                          40%
+                        </span>
+                      </p>
+                    </span>
+                    <span className="h-2 bg-gray-300 rounded-lg w-[95%] block">
+                      <span className="block w-[63%] h-2 justify-center bg-[#A0153E] rounded-lg"></span>
+                    <p className="text-gray-500 text-sm">Tersedia</p>
+                    </span>
+                  </div>
+                </div>
+                
               </div>
             </div>
           </main>
@@ -122,11 +194,25 @@ export default function Atas() {
               Lagi trending nih
             </h1>
             <div className="flex gap-4 flex-wrap">
-              <span className="w-[18rem] h-[7rem] bg-slate-500 flex gap-7 items-center rounded-lg">
-                <span className="bg-red-600 w-28 h-full block rounded-lg"></span>
+              <span className="w-[18rem] h-[7rem]  flex gap-5 items-center rounded-lg">
+                <Image src="/images/diskon_sepatu.png" alt="Diskon Sepatu" width={100} height={100} className="bg-red-600 w-28 h-full block rounded-lg"></Image>
                 <span className="flex flex-col gap-2">
-                  <h1>Judul Produk</h1>
-                  <p>12.6rb terjual</p>
+                  <h1 className="font-bold">Nike Shoes</h1>
+                  <p className="">12.6rb terjual</p>
+                </span>
+              </span>
+              <span className="w-[18rem] h-[7rem]  flex gap-5 items-center rounded-lg">
+                <Image src="/images/diskon_sepatu.png" alt="Diskon Sepatu" width={100} height={100} className="bg-red-600 w-28 h-full block rounded-lg"></Image>
+                <span className="flex flex-col gap-2">
+                  <h1 className="font-bold">Nike Shoes</h1>
+                  <p className="">12.6rb terjual</p>
+                </span>
+              </span>
+              <span className="w-[18rem] h-[7rem]  flex gap-5 items-center rounded-lg">
+                <Image src="/images/diskon_sepatu.png" alt="Diskon Sepatu" width={100} height={100} className="bg-red-600 w-28 h-full block rounded-lg"></Image>
+                <span className="flex flex-col gap-2">
+                  <h1 className="font-bold">Nike Shoes</h1>
+                  <p className="">12.6rb terjual</p>
                 </span>
               </span>
             </div>
@@ -136,21 +222,21 @@ export default function Atas() {
               For You
             </h1>
             <div className="">
-              <span className="flex w-[13rem] h-[16rem] bg-green-500 flex-col justify-between rounded-lg border-2 border-gray-300 ">
+              <span className="flex w-[18rem] h-[18rem] bg-green-500 flex-col justify-between rounded-lg border-2 border-gray-300 ">
                 <Image
-                  className="w-full h-32 bg-white rounded-lg rounded-br-3xl"
+                  className="w-full h-full bg-white rounded-lg rounded-br-3xl"
                   src="/images/kategori_topi.png"
                   alt="topi"
                   width={500}
                   height={500}
                 ></Image>
                 <h3>Topi Keren</h3>
-                <p>Rp 500.0000</p>
+                {/* <p>Rp 500.0000</p>
                 <span className="flex gap-3">
                   <span>Rating</span>
                   <span>|</span>
                   <span>30K terjual</span>
-                </span>
+                </span> */}
               </span>
             </div>
           </main>
