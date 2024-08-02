412259280062daca9a7c9eaaff8da8ad847786bc
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-08-02 13:46:26 +0700

    Menambah file konfigurasi firebase

---
+++ utils/firebase/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBdqIK0O22U0DR5aGz_GPFBGR_CatC6IqU",
+  authDomain: "shopin-ab966.firebaseapp.com",
+  projectId: "shopin-ab966",
+  storageBucket: "shopin-ab966.appspot.com",
+  messagingSenderId: "321008799505",
+  appId: "1:321008799505:web:bf6323295cc1b5b83186b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
