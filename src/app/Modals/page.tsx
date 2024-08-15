d778caac866552f0eb311004c1d4997ccfe94c34
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-08-15 22:07:31 +0700

    Update

--- src/app/Modals/page.tsx
+++ src/app/Modals/page.tsx
@@ -3,17 +3,22 @@ import { useEffect, useState } from "react";
 import { district, province, regency } from "../../../utils/fetchLocation";
 
 export default function locationFormModal() {
-  const [province, setProvince] = useState(Array);
+  const [provinces, setProvinces] = useState(Array);
+  const [idProvince, setIdProvince] = useState(0);
+  const [regencies, setRegencies] = useState(Array);
 
   useEffect(() => {
     async function getLocation() {
       const dataProvince = await province();
-      const dataRegency = await regency();
-      const dataDistrict = await district(dataRegency?.data.id);
-      setProvince([dataProvince, dataRegency, dataDistrict]);
+      dataProvince && setProvinces(dataProvince);
+      if(idProvince){
+        return const dataRegency = await regency(idProvince)}
+      dataRegency && setRegencies(dataRegency);
+      // const dataDistrict = await district(dataRegency?.data.id);
     }
     getLocation();
-  }, []);
+  }, [idProvince]);
+  console.log(regencies);
 
   return (
     <>
@@ -21,22 +26,23 @@ export default function locationFormModal() {
         <div className="">
           <h1>Modal</h1>
           <form className="flex flex-col" action="">
-            {province.length > 0 &&
-              province.map((data: any) => (
-                <>
-                  <label htmlFor="provinsi">{data.type}</label>
-                  <select
-                    name=""
-                    onChange={() => setIdProvince()}
-                    className="cursor-pointer"
-                    id=""
-                  >
-                    {data.data.map((loc: any) => (
-                      <option value="">{loc.name}</option>
-                    ))}
-                  </select>
-                </>
-              ))}
+            <label htmlFor="provinsi">Provinsi</label>
+            <select name="" className="cursor-pointer" onChange={(e:any)=>setIdProvince(e.target.value)} id="">
+              {provinces.length > 0 &&
+                provinces.map((data: any,i:number) => (
+                  <option key={i} value={data.id}>{data.name}</option>
+                ))}
+            </select>
+            {regencies.length>0&&(
+              <>
+              <label htmlFor="provinsi">Kota/Kabupaten</label>
+            <select name="" className="cursor-pointer" onChange={(e:any)=>setIdProvince(e.target.value)} id="">
+              {regencies.map((data: any,i:number) => (
+                  <option key={i} value={data.id}>{data.name}</option>
+                ))}
+            </select>
+            </>
+            )}
             <label htmlFor="Kode Pos"></label>
             <label htmlFor="">Alamat Lengkap :</label>
             <input type="text" name="" id="" />
--- utils/fetchLocation.ts
+++ utils/fetchLocation.ts
@@ -1,10 +1,9 @@
-
 export const province = async () => {
   const result = fetch(
     `https://www.emsifa.com/api-wilayah-indonesia/api/provinces.json`
   ).then(async (res) => {
     try {
-      const response = { type: "Provinsi", data: await res.json() };
+      const response = await res.json();
       return response;
     } catch (err) {
       console.log(err);
@@ -18,7 +17,7 @@ export const regency = (idRegency:number) => {
     `https://www.emsifa.com/api-wilayah-indonesia/api/regencies/${idRegency}.json`
   ).then(async (res) => {
     try {
-      const response = { type: "Provinsi", data: await res.json() };
+      const response = await res.json();
       return response;
     } catch (err) {
       console.log(err);
