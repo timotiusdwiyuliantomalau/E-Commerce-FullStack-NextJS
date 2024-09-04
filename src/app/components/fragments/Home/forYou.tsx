9a80a101dcbe92fe125d6523a30fa23b0e7b083e
timotiusdwiyuliantomalau <timotiusdym17@gmail.com>
2024-09-04 23:14:23 +0700

    Update UI

--- src/app/components/fragments/Home/forYou.tsx
+++ src/app/components/fragments/Home/forYou.tsx
@@ -1,65 +1,65 @@
-"use client";
-import Image from "next/image";
-import Link from "next/link";
-import React from "react";
-import { BsFillStarFill } from "react-icons/bs";
-import { GoVerified } from "react-icons/go";
-export default function ForYou() {
-  const arrProducts =
-    localStorage.getItem("products") &&
-    JSON.parse(localStorage.getItem("products") || "");
+// "use client";
+// import Image from "next/image";
+// import Link from "next/link";
+// import React from "react";
+// import { BsFillStarFill } from "react-icons/bs";
+// import { GoVerified } from "react-icons/go";
+// export default function ForYou() {
+//   const arrProducts =
+//     localStorage.getItem("products") &&
+//     JSON.parse(localStorage.getItem("products") || "");
 
-  return (
-    <>
-      <main className="w-full ">
-        <h1 className="text-left text-2xl font-semibold w-full mb-7">
-          Untuk Anda
-        </h1>
-        <div className="flex gap-8 flex-wrap">
-          {arrProducts &&
-            arrProducts?.map((products: any, index: number) =>
-              products.data?.map((product: any, i: number) => (
-                <span
-                  key={i}
-                  className="flex flex-col rounded-lg  w-[11rem] pb-5 border-2 border-gray-200 cursor-pointer group hover:scale-110"
-                >
-                  <Image
-                    className="h-[11rem] group rounded-lg rounded-br-[2.5em] bg-blue-900 object-cover object-center"
-                    src={product.product_photos[0]}
-                    alt={`Gambar ${index + 1}`}
-                    width={300}
-                    height={300}
-                  ></Image>
-                  <span className="pl-2 grid">
-                    <Link
-                      href={`detail/${product.product_id}`}
-                      className=" mb-2 mt-1 h-[2.5rem] text-sm overflow-hidden group-hover:underline"
-                    >
-                      {product.product_title}
-                    </Link>
-                    <p className="text-lg font-semibold mb-1">
-                      {product.typical_price_range != null
-                        ? product.typical_price_range[0]
-                        : "$32"}
-                    </p>
-                    <span className="flex gap-1 mb-1 items-center overflow-hidden">
-                      <GoVerified className="text-blue-400 w-4 h-4"></GoVerified>
-                      <p className="text-xs text-gray-600 h-4 w-full overflow-hidden">
-                        {product.offer.store_name}
-                      </p>
-                    </span>
-                    <span className="flex gap-1 text-xs items-center text-gray-600">
-                      <BsFillStarFill className="text-yellow-500 inline" />{" "}
-                      {product.product_rating}
-                      <span>|</span>
-                      <span>{product.product_num_reviews} terjual</span>
-                    </span>
-                  </span>
-                </span>
-              ))
-            )}
-        </div>
-      </main>
-    </>
-  );
-}
+//   return (
+//     <>
+//       <main className="w-full ">
+//         <h1 className="text-left text-2xl font-semibold w-full mb-7">
+//           For You
+//         </h1>
+//         <div className="flex gap-8 flex-wrap">
+//           {arrProducts &&
+//             arrProducts?.map((products: any, index: number) =>
+//               products.data?.map((product: any, i: number) => (
+//                 <span
+//                   key={i}
+//                   className="flex flex-col rounded-lg  w-[11rem] pb-5 border-2 border-gray-200 cursor-pointer group hover:scale-110"
+//                 >
+//                   <Image
+//                     className="h-[11rem] group rounded-lg rounded-br-[2.5em] bg-blue-900 object-cover object-center"
+//                     src={product.product_photos[0]}
+//                     alt={`Gambar ${index + 1}`}
+//                     width={300}
+//                     height={300}
+//                   ></Image>
+//                   <span className="pl-2 grid">
+//                     <Link
+//                       href={`detail/${product.product_id}`}
+//                       className=" mb-2 mt-1 h-[2.5rem] text-sm overflow-hidden group-hover:underline"
+//                     >
+//                       {product.product_title}
+//                     </Link>
+//                     <p className="text-lg font-semibold mb-1">
+//                       {product.typical_price_range != null
+//                         ? product.typical_price_range[0]
+//                         : "$32"}
+//                     </p>
+//                     <span className="flex gap-1 mb-1 items-center overflow-hidden">
+//                       <GoVerified className="text-blue-400 w-4 h-4"></GoVerified>
+//                       <p className="text-xs text-gray-600 h-4 w-full overflow-hidden">
+//                         {product.offer.store_name}
+//                       </p>
+//                     </span>
+//                     <span className="flex gap-1 text-xs items-center text-gray-600">
+//                       <BsFillStarFill className="text-yellow-500 inline" />{" "}
+//                       {product.product_rating}
+//                       <span>|</span>
+//                       <span>{product.product_num_reviews} terjual</span>
+//                     </span>
+//                   </span>
+//                 </span>
+//               ))
+//             )}
+//         </div>
+//       </main>
+//     </>
+//   );
+// }
